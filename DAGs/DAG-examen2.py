6745bf89fb8bd4418676c09e609d6c6a83931248
NicoSoriaPrueba <93299287+NicoSoriaPrueba@users.noreply.github.com>
2021-10-29 15:38:30 -0300

    dag 2

---
+++ DAGs/DAG-examen2.py
@@ -0,0 +1,56 @@
+import datetime
+import os
+from airflow import models
+from airflow.contrib.operators import dataproc_operator
+from airflow.utils import trigger_rule
+
+yesterday = datetime.datetime.combine(
+    datetime.datetime.today() - datetime.timedelta(1),
+    datetime.datetime.min.time())
+
+default_dag_args = {
+    # Setting start date as yesterday starts the DAG immediately when it is
+    # detected in the Cloud Storage bucket.
+    'start_date': yesterday,
+    # To email on failure or retry set 'email' arg to your email and enable
+    # emailing here.
+    'email_on_failure': False,
+    'email_on_retry': False,
+    # If a task fails, retry it once after waiting at least 5 minutes
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+    'project_id': 'test-opi-330322'
+}
+
+CLUSTER_NAME = 'airflow-examen'
+REGION='us-central1'
+#PROJECT_ID='test-opi-330322'
+PYSPARK_URI='~/examen/ETL/loadBQ.py'
+
+with models.DAG(
+        'composer_sample_quickstart',
+        # Continue to run DAG once per day
+        schedule_interval=datetime.timedelta(days=1),
+        default_args=default_dag_args) as dag:
+    # Create a Cloud Dataproc cluster.
+    create_dataproc_cluster = dataproc_operator.DataprocClusterCreateOperator(
+        task_id='create_dataproc_cluster',
+        # Give the cluster a unique name by appending the date scheduled.
+        # See https://airflow.apache.org/code.html#default-variables
+        cluster_name='composer-hadoop-tutorial-cluster',
+        num_workers=2,
+        region='us-central1',
+        zone='us-central1-a',
+        image_version='2.0',
+        master_machine_type='n1-standard-2',
+        worker_machine_type='n1-standard-2')
+    # Delete Cloud Dataproc cluster.
+    delete_dataproc_cluster = dataproc_operator.DataprocClusterDeleteOperator(
+        task_id='delete_dataproc_cluster',
+        region='us-central1',
+        cluster_name='composer-hadoop-tutorial-cluster',
+        # Setting trigger_rule to ALL_DONE causes the cluster to be deleted
+        # even if the Dataproc job fails.
+        trigger_rule=trigger_rule.TriggerRule.ALL_DONE)
+    # Define DAG dependencies.
+    create_dataproc_cluster >> delete_dataproc_cluster
